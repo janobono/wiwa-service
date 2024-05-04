792d246762c99cfe8e9e26f3af662aff921248ff
janobono <janobono@gmail.com>
2024-05-04 17:41:09 +0200

    Order summary
    Signed-off-by: janobono <janobono@gmail.com>

--- wiwa-service-business/wiwa-service-business-impl/src/test/java/sk/janobono/wiwa/business/impl/component/SummaryUtilTest.java
+++ wiwa-service-business/wiwa-service-business-impl/src/test/java/sk/janobono/wiwa/business/impl/component/SummaryUtilTest.java
@@ -7,15 +7,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import sk.janobono.wiwa.business.model.DimensionsData;
 import sk.janobono.wiwa.business.model.application.ManufacturePropertiesData;
+import sk.janobono.wiwa.business.model.application.PriceForCuttingData;
+import sk.janobono.wiwa.business.model.application.PriceForGluingEdgeData;
+import sk.janobono.wiwa.business.model.application.PriceForGluingLayerData;
 import sk.janobono.wiwa.business.model.order.OrderBoardData;
+import sk.janobono.wiwa.business.model.order.OrderEdgeData;
 import sk.janobono.wiwa.business.model.order.part.PartData;
-import sk.janobono.wiwa.business.model.order.summary.OrderItemSummaryData;
+import sk.janobono.wiwa.business.model.order.summary.*;
 import sk.janobono.wiwa.dal.domain.OrderItemSummaryDo;
+import sk.janobono.wiwa.dal.domain.OrderSummaryViewDo;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,7 +28,8 @@ class SummaryUtilTest {
 
     private ObjectMapper objectMapper;
 
-    private Map<Long, OrderBoardData> boards;
+    private List<OrderBoardData> boards;
+    private List<OrderEdgeData> edges;
     private ManufacturePropertiesData manufactureProperties;
 
     private SummaryUtil summaryUtil;
@@ -35,14 +40,23 @@ class SummaryUtilTest {
                 .addModule(new JavaTimeModule())
                 .build();
 
-        boards = Map.of(
-                1L, OrderBoardData.builder()
+        boards = List.of(
+                OrderBoardData.builder()
                         .id(1L)
                         .orientation(false)
-                        .weight(new BigDecimal("15.5"))
+                        .weight(BigDecimal.valueOf(1))
                         .length(BigDecimal.valueOf(2000))
                         .width(BigDecimal.valueOf(1500))
                         .thickness(BigDecimal.valueOf(10))
+                        .price(BigDecimal.valueOf(100))
+                        .build()
+        );
+
+        edges = List.of(
+                OrderEdgeData.builder()
+                        .id(1L)
+                        .weight(BigDecimal.ONE)
+                        .width(BigDecimal.valueOf(18))
                         .price(BigDecimal.valueOf(100))
                         .build()
         );
@@ -70,11 +84,53 @@ class SummaryUtilTest {
         final PartData part = objectMapper.readValue(getClass().getResource("/part_basic.json"), PartData.class);
         final OrderItemSummaryData orderItemSummary = summaryUtil.calculateItemSummary(part,
                 1,
-                boards.values().stream().collect(Collectors.toMap(OrderBoardData::id, OrderBoardData::thickness)),
+                boards.stream().collect(Collectors.toMap(OrderBoardData::id, OrderBoardData::thickness)),
                 manufactureProperties);
 
         final List<OrderItemSummaryDo> items = summaryUtil.toOrderItemSummaries(1L, orderItemSummary);
 
         assertThat(orderItemSummary).usingRecursiveComparison().isEqualTo(summaryUtil.toOrderItemSummary(items));
+    }
+
+    @Test
+    void toOrderSummary_whenPartBasic_thenTheseResults() throws IOException {
+        final PartData part = objectMapper.readValue(getClass().getResource("/part_basic.json"), PartData.class);
+        final OrderItemSummaryData orderItemSummary = summaryUtil.calculateItemSummary(part,
+                1,
+                boards.stream().collect(Collectors.toMap(OrderBoardData::id, OrderBoardData::thickness)),
+                manufactureProperties);
+
+        final List<OrderItemSummaryDo> items = summaryUtil.toOrderItemSummaries(1L, orderItemSummary);
+
+        final OrderSummaryData orderSummary = summaryUtil.toOrderSummary(
+                boards,
+                edges,
+                BigDecimal.valueOf(20),
+                List.of(
+                        new PriceForCuttingData(BigDecimal.valueOf(10), BigDecimal.valueOf(1))
+                ),
+                new PriceForGluingLayerData(BigDecimal.valueOf(1)),
+                List.of(
+                        new PriceForGluingEdgeData(BigDecimal.valueOf(20), BigDecimal.valueOf(1))
+                ),
+                items.stream()
+                        .map(item -> new OrderSummaryViewDo(1L, item.getCode(), item.getAmount()))
+                        .toList()
+        );
+
+        assertThat(orderSummary.boardSummary().size()).isEqualTo(1);
+        assertThat(orderSummary.boardSummary().getFirst()).usingRecursiveComparison().isEqualTo(new OrderBoardSummaryData(1L, new BigDecimal("0.250"), BigDecimal.valueOf(1), new BigDecimal("0.083"), BigDecimal.valueOf(100), new BigDecimal("120.000")));
+
+        assertThat(orderSummary.edgeSummary().size()).isEqualTo(1);
+        assertThat(orderSummary.edgeSummary().getFirst()).usingRecursiveComparison().isEqualTo(new OrderEdgeSummaryData(1L, new BigDecimal("2.620"), new BigDecimal("2.300"), new BigDecimal("2.620"), new BigDecimal("262.000"), new BigDecimal("314.400"), new BigDecimal("2.300"), new BigDecimal("2.760")));
+
+        assertThat(orderSummary.glueSummary()).usingRecursiveComparison().isEqualTo(new OrderGlueSummaryData(BigDecimal.valueOf(0), BigDecimal.valueOf(0), new BigDecimal("0.000")));
+
+        assertThat(orderSummary.cutSummary().size()).isEqualTo(1);
+        assertThat(orderSummary.cutSummary().getFirst()).usingRecursiveComparison().isEqualTo(new OrderCutSummaryData(BigDecimal.valueOf(10), new BigDecimal("2.300"), new BigDecimal("2.300"), new BigDecimal("2.760")));
+
+        assertThat(orderSummary.weight()).isEqualTo(new BigDecimal("2.703"));
+        assertThat(orderSummary.total()).isEqualTo(new BigDecimal("366.600"));
+        assertThat(orderSummary.vatTotal()).isEqualTo(new BigDecimal("439.920"));
     }
 }
